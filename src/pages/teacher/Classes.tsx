687f82f91c352221bffdd3e0c91a53c9a4e83a65
MuhammadOsman381 <mosman257@gmail.com>
2025-04-11 05:45:26 +0500

    class issue:fixed

--- src/pages/teacher/Classes.tsx
+++ src/pages/teacher/Classes.tsx
@@ -1,29 +1,29 @@
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table";
+// import {
+//     Table,
+//     TableBody,
+//     TableCaption,
+//     TableCell,
+//     TableHead,
+//     TableHeader,
+//     TableRow,
+// } from "@/components/ui/table";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import useGetAndDelete from "@/hooks/useGetAndDelete";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import SpinnerLoader from "@/components/SpinLoader";
+// import SpinnerLoader from "@/components/SpinLoader";
 import { Button } from "@/components/ui/button";
-import { ArrowLeft } from "lucide-react";
+// import { ArrowLeft } from "lucide-react";
 import { Label } from "@radix-ui/react-dropdown-menu";
 import { Input } from "@/components/ui/input";
-import usePostAndPut from "@/hooks/usePostAndPut";
+// import usePostAndPut from "@/hooks/usePostAndPut";
 
 
 const Classes = () => {
     const [teacherName, setTeacherName] = useState<string>("");
     const [teacherEmail, setTeacherEmail] = useState<string>("");
-    const [teacherPassword, setTeacherPassword] = useState<string>("");
-    const [courses, setCourses] = useState<{ id: string, name: string }[]>([]);
+    // const [teacherPassword, setTeacherPassword] = useState<string>("");
+    // const [courses, setCourses] = useState<{ id: string, name: string }[]>([]);
     const getCourse = useGetAndDelete(axios.get);
 
     useEffect(() => {
@@ -31,7 +31,7 @@ const Classes = () => {
             const response = await getCourse.callApi("course/get-teacher-courses-time", true, false);
             console.log(response)
             if (response?.course) {
-                setCourses(response.course);
+                // setCourses(response.course);
             }
         };
         getCourses();
